4d38ccc7dc8237cbf23cbb67196cbbf0ff06d721
Andreas Motl <andreas.motl@crate.io>
2025-04-17 01:38:24 +0200

    Model: Update download URL for llms-ctx.txt

--- src/cratedb_about/model.py
+++ src/cratedb_about/model.py
@@ -34,7 +34,7 @@ class Settings:
 
     llms_txt_url = os.getenv(
         "CRATEDB_CONTEXT_URL",
-        "https://raw.githubusercontent.com/crate/about/6876fedee57f59b693f37996e04f53c6446f2ad6/build/llm/llms-ctx.txt",
+        "https://raw.githubusercontent.com/crate/about/v0.0.1/build/llm/llms-ctx.txt",
     )
     instructions = "You are a helpful and concise assistant."
     llms_txt = None
