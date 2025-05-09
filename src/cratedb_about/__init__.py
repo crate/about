b24eff6580f4bfd36c29ad728c59a0772445b8d1
Andreas Motl <andreas.motl@crate.io>
2025-05-10 01:10:04 +0200

    Chore: Fix packaging

--- src/cratedb_about/__init__.py
+++ src/cratedb_about/__init__.py
@@ -1,5 +1,14 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .outline import CrateDbKnowledgeOutline
 
 __all__ = [
     "CrateDbKnowledgeOutline",
 ]
+
+__appname__ = "cratedb-about"
+
+try:
+    __version__ = version(__appname__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
