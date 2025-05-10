1f4cb68e0fa2685489b404e1f6da1a6f8d1779e5
Andreas Motl <andreas.motl@crate.io>
2025-05-10 04:39:30 +0200

    Refactor LLM conversations code to `CrateDbKnowledgeConversation`

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,5 @@ import pytest
 @pytest.fixture(scope="session", autouse=True)
 def prune_environ():
     """Prevent environment variables from leaking into software tests"""
+    os.environ.pop("OPENAI_API_KEY", None)
     os.environ.pop("OUTDIR", None)
