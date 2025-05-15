42ca23c162a80dac575cfd143322a7895d92915d
Andreas Motl <andreas.motl@crate.io>
2025-05-15 23:59:46 +0200

    Bundle: Provide README in HTML format per `readme.html`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -62,6 +62,8 @@ def test_cli_bundle_success(caplog, tmp_path):
     assert "Ready." in caplog.text
 
     # Verify that the expected output files have been created.
+    assert (tmp_path / "readme.md").exists()
+    assert (tmp_path / "readme.html").exists()
     assert (tmp_path / "llms.txt").exists()
     assert (tmp_path / "llms-full.txt").exists()
 
