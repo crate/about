0e70c049d59220504e6addf4461cbda89b0e4498
Andreas Motl <andreas.motl@crate.io>
2025-07-21 17:26:58 +0200

    Prompt: Naming things. Use `GeneralInstructions` as class name.

--- tests/test_instructions.py
+++ tests/test_instructions.py
@@ -1,8 +1,8 @@
-from cratedb_about.instruction import Instructions
+from cratedb_about.instruction import GeneralInstructions
 
 
 def test_instructions_full():
-    instructions_text = Instructions.full()
+    instructions_text = GeneralInstructions().render()
     assert "Things to remember when working with CrateDB" in instructions_text
     assert "Rules for writing SQL queries" in instructions_text
     assert "Core writing principles" in instructions_text
